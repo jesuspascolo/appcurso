4f25d877839d8b2557a927b088351f6dd5aac1ea
Gonzalez Pascolo <jesus-david.gonzalezpascolo.external@atos.net>
2018-06-18 15:58:38 +0200

    Data Binding finalizado

---
+++ src/app/ejtwowaybinding/ejtwowaybinding.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ejtwowaybinding',
+  templateUrl: './ejtwowaybinding.component.html',
+  styleUrls: ['./ejtwowaybinding.component.css']
+})
+export class EjtwowaybindingComponent implements OnInit {
+
+  texto = 'Texto original';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
